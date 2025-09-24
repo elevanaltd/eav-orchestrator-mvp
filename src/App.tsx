f26afe516d99376d4be3bbff4632384d97b50b3a
Shaun Buswell <shaun.buswell@elevana.com>
2025-09-25 00:49:59 +0100

    feat: TestRLS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { TipTapEditor } from './components/TipTapEditor'
+import { TestRLS } from './components/TestRLS'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <TestRLS />
       <TipTapEditor />
     </div>
   )
