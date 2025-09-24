c11f3d4cc15bd8c6972ea436b5f3b996e13bc912
Shaun Buswell <shaun.buswell@elevana.com>
2025-09-25 00:55:04 +0100

    feat: wire up auth routing in App
    - Added BrowserRouter with routes for login/signup
    - Protected main app behind PrivateRoute
    - AuthProvider wraps entire app for auth state
    - Added basic test for app rendering with auth
    - Separated MainApp component for authenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import { Login } from './components/auth/Login'
+import { Signup } from './components/auth/Signup'
+import { PrivateRoute } from './components/auth/PrivateRoute'
 import { TipTapEditor } from './components/TipTapEditor'
 import { TestRLS } from './components/TestRLS'
 import './App.css'
 
-function App() {
+function MainApp() {
   return (
     <div className="App">
       <TestRLS />
       <TipTapEditor />
     </div>
+  )
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <MainApp />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
